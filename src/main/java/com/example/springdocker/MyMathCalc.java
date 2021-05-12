f122368bbad2c69eb9d3cfba289786ae3d8cba24
Vilma Kaijser <vilma.kaijser@gmail.com>
2021-05-12 14:38:35 +0200

    added mathcalc class

---
+++ src/main/java/com/example/springdocker/MyMathCalc.java
@@ -0,0 +1,22 @@
+package com.example.springdocker;
+
+/**
+ * Created by Vilma Couturier Kaijser
+ * Date: 2021-05-12
+ * Project: spring-docker-demo
+ * Copyright: MIT
+ */
+public class MyMathCalc {
+
+    public int add(int a, int b){
+        return a + b;
+    }
+
+    public int multiply(int a, int b){
+        return a * b;
+    }
+
+    public int divide(int a, int b){
+        return a/b;
+    }
+}
